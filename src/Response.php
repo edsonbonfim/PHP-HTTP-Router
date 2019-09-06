c0c832b35a901c4389888b1fc0ced80a66159065
Edson Onildo <inbox.edsononildo@gmail.com>
2019-09-06 00:06:38 -0300

    Removendo tipagem

--- src/Response.php
+++ src/Response.php
@@ -4,7 +4,7 @@ namespace Bonfim\Router;
 
 class Response
 {
-    public function withJson(array $json, int $code = 200)
+    public function withJson($json, $code = 200)
     {
         echo json_encode($json);
         return http_response_code($code);
